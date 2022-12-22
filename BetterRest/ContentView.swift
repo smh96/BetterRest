77e0663d5af0de51fa0e23d866a20fd18ece9324
Sander Haug <sander_haug@hotmail.com>
2022-12-22 20:04:41 +0100

    Amend

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(Date.now.formatted(date: .long, time: .omitted))
+    }
+    
+    func trivialExample() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
